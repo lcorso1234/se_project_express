7c0d34894b29fef20bcf349b597ea28c57ce7b60
lcorso1234 <lawrencecorso1@gmail.com>
2026-01-27 11:11:33 -0600

    Love9

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.use(cors());
 // Log all requests and responses
 app.use(requestLogger);
 
+// Temporary user ID for testing purposes
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133"
+  };
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
