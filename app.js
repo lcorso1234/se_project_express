aec073d547523e5418bd802cab32e13d93376f81
lcorso1234 <lawrencecorso1@gmail.com>
2026-01-30 10:52:20 -0600

    Love14

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -5,8 +7,6 @@ const { errors } = require('celebrate');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-
-require('dotenv').config();
 
 mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
 
@@ -17,14 +17,6 @@ app.use(cors());
 
 // Log all requests and responses
 app.use(requestLogger);
-
-// Temporary user ID for testing purposes
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5d8b8592978f8bd833ca8133',
-  };
-  next();
-});
 
 app.get('/', (req, res) => {
   res.send('Server is running');
